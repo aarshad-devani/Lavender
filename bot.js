9f85eafff1dda40cb9ab90bd6eec27cec20dcf4d
Matt <wowitbematt@gmail.com>
2018-09-28 23:20:34 -0700

    Updated to fit changes

--- bot.js
+++ bot.js
@@ -3,24 +3,21 @@ const fs = require("fs");
 const yt = require("ytdl-core");
 const config = require("./config.json");
 const client = new Discord.Client();
-const prefix = config.prefix;
 const request = require("request");
-const rn = require("random-number");
 const https = require('https');
 
 client.on('ready', () => {
 	console.log("Logged in as Miku-Chan!");
-	client.user.setGame("Use m!help");
+	client.user.setActivity("Use m!help");
 	console.log("Presence Changed.");
 	console.log("Using Version 1.1-rc. This may be edited than the original tag.");
 	console.log("-------");
 });
 
-client.on('msg', msg => {
-
+client.on('message', message => {
 	function help() {
 		// Clamp 2 commands to a line?
-		msg.channel.send({embed: {
+		message.channel.send({embed: {
 			color: 3447003,
 			author: {
 				name: client.user.username,
@@ -28,62 +25,62 @@ client.on('msg', msg => {
 			},
 			title: "Miku-Chan - Help",
 			url: "http://github.com/Miku-Chan-Devs/Miku-Chan/wiki/Commands",
-			description: `Miku-Chan's commands are as follows. The prefix is ${config.prefix}.`,
+			description: `Miku-Chan's commands are as follows. The "" + config.prefix + is ${config.prefix}.`,
 			fields: [
 			{
-				name: prefix + "8ball",
+				name: "" + config.prefix + "8ball",
 				value: "It's an 8ball..."
 			},
 			{
-				name: prefix + "help, m!h, m!cmds",
+				name: "" + config.prefix + "help, m!h, m!cmds",
 				value: "See the commands that Miku-Chan understands"
 			},
 			{
-				name: prefix + "icup",
+				name: "" + config.prefix + "icup",
 				value: "I C U P"
 			},
 			{
-				name: prefix + "about",
+				name: "" + config.prefix + "about",
 				value: "Learn about Miku-Chan"
 			},
 			{
-				name: prefix + "google",
+				name: "" + config.prefix + "google",
 				value: "Google what's after the command"
 			},
 			{
-				name: prefix + "pocketmonster",
+				name: "" + config.prefix + "pocketmonster",
 				value: "Search the Pocket Monster Directory"
 			},
 			{
-				name: prefix + "tf, m!uf",
+				name: "" + config.prefix + "tf, m!uf",
 				value: "Animate a tableflip and un-flipping a table"
 			},
 			{
-				name: prefix + "botisdead",
+				name: "" + config.prefix + "botisdead",
 				value: "Get a Link to the Miku-Chan status page"
 			},
 			{
-				name: prefix + "ping, m!ding",
+				name: "" + config.prefix + "ping, m!ding",
 				value: "Get your ping, or just ding"
 			},
 			{
-				name: prefix + "join",
+				name: "" + config.prefix + "join",
 				value: "Join the Current Voice Channel"
 			},
 			{
-				name: prefix + "add",
+				name: "" + config.prefix + "add",
 				value: "Add to the Queue"
 			},
 			{
-				name: prefix + "queue",
+				name: "" + config.prefix + "queue",
 				value: "See the Queue"
 			},
 			{
-				name: prefix + "play",
+				name: "" + config.prefix + "play",
 				value: "Play a song from YouTube"
 			},
 			{
-				name: prefix + "crypto",
+				name: "" + config.prefix + "crypto",
 				value: "Add a space and add the crypto abbreviation"
 			}],
 			timestamp: new Date(),
@@ -94,105 +91,105 @@ client.on('msg', msg => {
 	}});
 }
 
-	if(msg == prefix + "play") {
+	if(message == "" + config.prefix + "play") {
 		// Not ready. "'queue' is not defined. (no-undef)" - Codacy
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`I'm not a magician. Add some songs using ${prefix}add`);
-		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
-		if (queue[msg.guild.id].playing) {
-			msg.channel.sendMessage('Already playing music, idiot.');
+		if (queue[message.guild.id] === undefined) return message.channel.sendMessage(`I'm not a magician. Add some songs using ${prefix}add`);
+		if (!message.guild.voiceConnection) return commands.join(message).then(() => commands.play(message));
+		if (queue[message.guild.id].playing) {
+			message.channel.sendMessage('Already playing music, idiot.');
 		}
 		let dispatcher;
-		queue[msg.guild.id].playing = true;
+		queue[message.guild.id].playing = true;
 		console.log(queue);
 		(function play(song) {
 			console.log(song);
 			if (song === undefined) {
-				msg.channel.sendMessage('The queue\'s empty, you know?').then(() => {
-					queue[msg.guild.id].playing = false;
-					msg.member.voiceChannel.leave();
+				message.channel.sendMessage('The queue\'s empty, you know?').then(() => {
+					queue[message.guild.id].playing = false;
+					message.member.voiceChannel.leave();
 				});
 			}
-			msg.channel.sendMessage(`Playing ***${song.title}*** as requested by ***${song.requester}***`);
-			dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : config.passes });
-			let collector = msg.channel.createCollector(m => m);
-			collector.on("msg", m => {
-				if (m.content.startsWith(prefix + 'pause')) {
-					msg.channel.sendMessage('paused').then(() => {dispatcher.pause();});
-				} else if (m.content.startsWith(prefix + 'resume')){
-					msg.channel.sendMessage('resumed').then(() => {dispatcher.resume();});
-				} else if (m.content.startsWith(prefix + 'skip')){
-					msg.channel.sendMessage('skipped').then(() => {dispatcher.end();});
-				} else if (m.content.startsWith(prefix + 'time')){
-					msg.channel.sendMessage(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+			message.channel.sendMessage(`Playing ***${song.title}*** as requested by ***${song.requester}***`);
+			dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : config.passes });
+			let collector = message.channel.createCollector(m => m);
+			collector.on("message", m => {
+				if (m.content.startsWith("" + config.prefix + 'pause')) {
+					message.channel.sendMessage('paused').then(() => {dispatcher.pause();});
+				} else if (m.content.startsWith("" + config.prefix + 'resume')){
+					message.channel.sendMessage('resumed').then(() => {dispatcher.resume();});
+				} else if (m.content.startsWith("" + config.prefix + 'skip')){
+					message.channel.sendMessage('skipped').then(() => {dispatcher.end();});
+				} else if (m.content.startsWith("" + config.prefix + 'time')){
+					message.channel.sendMessage(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
 				}
 			});
 			dispatcher.on('end', () => {
 				collector.stop();
-				play(queue[msg.guild.id].songs.shift());
+				play(queue[message.guild.id].songs.shift());
 			});
 			dispatcher.on('error', (err) => {
-				return msg.channel.sendMessage('We have an error, guys. ' + err).then(() => {
+				return message.channel.sendMessage('We have an error, guys. ' + err).then(() => {
 					collector.stop();
-					play(queue[msg.guild.id].songs.shift());
+					play(queue[message.guild.id].songs.shift());
 				});
 			});
-		})(queue[msg.guild.id].songs.shift());
+		})(queue[message.guild.id].songs.shift());
 	}
-	if(msg == prefix + "join") {
+	if(message == "" + config.prefix + "join") {
 		// Ready for V2
 		return new Promise((resolve, reject) => {
-			const voiceChannel = msg.member.voiceChannel;
-			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.sendMessage('I can\'t get there, dude. Help me out?');
+			const voiceChannel = message.member.voiceChannel;
+			if (!voiceChannel || voiceChannel.type !== 'voice') return message.sendMessage('I can\'t get there, dude. Help me out?');
 			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
 		});
 	}
-	if(msg == prefix + "add") {
+	if(message == "" + config.prefix + "add") {
 		// Ready for V2
-		let url = msg.content.split(' ')[1];
-		if (url == '' || url === undefined) return msg.channel.sendMessage(`You need a YouTube URL or ID after ${prefix}add`);
+		let url = message.content.split(' ')[1];
+		if (url == '' || url === undefined) return message.channel.sendMessage(`You need a YouTube URL or ID after ${prefix}add`);
 		yt.getInfo(url, (err, info) => {
-			if(err) return msg.channel.sendMessage('You gave me a broken link, dude. ' + err);
-			if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
-			queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
-			msg.channel.sendMessage(`Added ***${info.title}*** to the queue`);
+			if(err) return message.channel.sendMessage('You gave me a broken link, dude. ' + err);
+			if (!queue.hasOwnProperty(message.guild.id)) queue[message.guild.id] = {}, queue[message.guild.id].playing = false, queue[message.guild.id].songs = [];
+			queue[message.guild.id].songs.push({url: url, title: info.title, requester: message.author.username});
+			message.channel.sendMessage(`Added ***${info.title}*** to the queue`);
 		});
 	}
-	if(msg == prefix + "queue") {
+	if(message == "" + config.prefix + "queue") {
 		// Ready for V2
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`The queue\'s empty, you know? Fill her up using ${prefix}add`);
+		if (queue[message.guild.id] === undefined) return message.channel.sendMessage(`The queue\'s empty, you know? Fill her up using ${prefix}add`);
 		let tosend = [];
-		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ***${song.title}*** - Requested by: ***${song.requester}***`);});
-		msg.channel.sendMessage(`__**${msg.guild.name}'s Music Queue:**__ Currently ***${tosend.length}*** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
+		queue[message.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ***${song.title}*** - Requested by: ***${song.requester}***`);});
+		message.channel.sendMessage(`__**${message.guild.name}'s Music Queue:**__ Currently ***${tosend.length}*** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
 	}
-	if(msg == prefix + "ping") {
+	if(message == "" + config.prefix + "ping") {
 		// Ready for V2
-		var ping = new Date().getTime() - msg.createdTimestamp + " ms";
-		msg.channel.send("Pong! The last ping was " + client.ping + " ms.");
+		var ping = new Date().getTime() - message.createdTimestamp + " ms";
+		message.channel.send("Pong! The last ping was " + client.ping + " ms.");
 	}
-	if(msg == prefix + "ding") {
+	if(message == "" + config.prefix + "ding") {
 		// Ready for V2
-		msg.channel.send('Dong!');
+		message.channel.send('Dong!');
 	}
-	if(msg == prefix + "cmds") {
-		// Ready for V2
-		help();
-	}
-	if(msg == prefix + "help") {
+	if(message == "" + config.prefix + "cmds") {
 		// Ready for V2
 		help();
 	}
-	if(msg == prefix + "h") {
+	if(message == "" + config.prefix + "help") {
 		// Ready for V2
 		help();
 	}
-	if(msg == prefix + "icup") {
+	if(message == "" + config.prefix + "h") {
 		// Ready for V2
-		msg.channel.send('Ha ha. Very funny. ***(not)***.');
+		help();
 	}
-	if(msg == prefix + "about") {
+	if(message == "" + config.prefix + "icup") {
 		// Ready for V2
-		if (msg.content === '$about') {
-			msg.channel.send({embed: {
+		message.channel.send('Ha ha. Very funny. ***(not)***.');
+	}
+	if(message == "" + config.prefix + "about") {
+		// Ready for V2
+		if (message.content === '$about') {
+			message.channel.send({embed: {
 				color: 3447003,
 				author: {
 					name: client.user.username,
@@ -203,7 +200,7 @@ client.on('msg', msg => {
 				description: "Miku-Chan is a Discord.JS Discord bot made to increase the autonomy of your server.",
 				fields: [{
 					name: "Some Commands",
-					value: "You can put the prefix (which is m!) in front of $cmds for commands."
+					value: "You can put the " + config.prefix + " (which is m!) in front of $cmds for commands."
 				},
 				{
 					name: "Source Code",
@@ -221,11 +218,11 @@ client.on('msg', msg => {
 			}});
 		}
 	}
-	if(msg == prefix + "google") {
+	if(message == "" + config.prefix + "google") {
 		// Broken. Doesn't Crash Bot (to testing knowledge)
-		const args = msg.content.slice(prefix).trim().split(/ +/g);
+		const args = message.content.slice(prefix).trim().split(/ +/g);
 		let query = args[0];
-		msg.channel.send({embed: {
+		message.channel.send({embed: {
 			color: 3447003,
 			title: "Your Search",
 			url: `https://www.google.com/search?hl=en_US&q=` + args.toString().replace(/,/g, '+') + ')',
@@ -237,10 +234,10 @@ client.on('msg', msg => {
 			},
 		}});
 	}
-	if(msg == prefix + "pocketmonster") {
+	if(message == "" + config.prefix + "pocketmonster") {
 		// Broken. Doesn't Crash Bot (to testing knowledge)
-		const args = msg.content.slice(prefix).trim().split(/ + /g);
-		msg.channel.send({embed: {
+		const args = message.content.slice(prefix).trim().split(/ + /g);
+		message.channel.send({embed: {
 			color: 3447003,
 			title: "Your Search",
 			url: `https://bulbapedia.bulbagarden.net/w/index.php?title=Special:Search&go=Go&searchToken=75r5fsf9yrqhcfdj8jul3tfwn&search=` + args.toString().replace(/,/g, '+') + ' ',
@@ -252,9 +249,9 @@ client.on('msg', msg => {
 		},
 		}});
 	}
-	if(msg == prefix + "tf") {
+	if(message == "" + config.prefix + "tf") {
 		// Ready for V2
-		msg.channel.send("(°-°)\\ ┬─┬").then(m => {
+		message.channel.send("(°-°)\\ ┬─┬").then(m => {
 			setTimeout(() => {
 				m.edit("(╯°□°)╯    ]").then(ms => {
 					setTimeout(() => {
@@ -264,9 +261,9 @@ client.on('msg', msg => {
 			}, 500);
 		});
 	}
-	if(msg == prefix + "uf") {
+	if(message == "" + config.prefix + "uf") {
 		// Ready for V2
-		msg.channel.send("(╯°□°)╯  ︵  ┻━┻").then(m => {
+		message.channel.send("(╯°□°)╯  ︵  ┻━┻").then(m => {
 			setTimeout(() => {
 				m.edit("(╯°□°)╯    ]").then(ms => {
 					setTimeout(() => {
@@ -276,16 +273,16 @@ client.on('msg', msg => {
 			}, 500);
 		});
 	}
-	if(msg == prefix + "botisdead") {
+	if(message == "" + config.prefix + "botisdead") {
 		// Ready for V2
-		msg.channel.send("If the bot is offline, visit http://status.mikuchan.me to find out when the bot comes back up again.");
+		message.channel.send("If the bot is offline, visit http://status.mikuchan.me to find out when the bot comes back up again.");
 	}
-	if(msg == prefix + "8ball") {
+	if(message == "" + config.prefix + "8ball") {
 		// Ready for V2
 		var responses = ["It is certain", "Without a doubt", "You may rely on it", "Most likely", "Yes", "Signs point to yes", "Better not tell you now", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
-		msg.channel.send(":8ball: " + responses[Math.floor(Math.random() * (responses.length))]);
+		message.channel.send(":8ball: " + responses[Math.floor(Math.random() * (responses.length))]);
 	}
-  if(msg == prefix + "crypto grlc") {
+  if(message == "" + config.prefix + "crypto grlc") {
 		// Ready for V2
 		var tokenURL = 'https://cryptocoincharts.info/fast/secret-api/pricing.php?coin=grlc&apiKey=djde93dekd94jwowqpjfngn';
 		https.get(tokenURL, function (res) {
@@ -299,14 +296,14 @@ client.on('msg', msg => {
                 var pricePart = priceResponse.price_usd;
                 pricePart = parseInt(pricePart*100)/100.0;
                 price = pricePart + "$ USD";
-								msg.channel.send("The price of Garlicoin (GRLC) is " + price + " per coin")
+								message.channel.send("The price of Garlicoin (GRLC) is " + price + " per coin")
                 console.log("Set Price: ", price);
             });
         }).on('error', (e) => {
             console.error(e);
 		});
   }
-  if(msg == prefix + "crypto btc") {
+  if(message == "" + config.prefix + "crypto btc") {
 	  var tokenURL = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD';
 	  https.get(tokenURL, function (res) {
              var body = '';
@@ -317,14 +314,14 @@ client.on('msg', msg => {
                  var priceResponse = JSON.parse(body);
                  console.log("Got a response: ", priceResponse);
                  var pricePart = priceResponse.USD;
-		 msg.channel.send("The price of Bitcoin (BTC) is " + pricePart + "$ per coin")
+		 message.channel.send("The price of Bitcoin (BTC) is " + pricePart + "$ per coin")
                  console.log("Set Price: ", pricePart);
              });
          }).on('error', (e) => {
              console.error(e);
 	  });
   }
-  if(msg == prefix + "crypto eth") {
+  if(message == "" + config.prefix + "crypto eth") {
 	  var tokenURL = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD';
 	  https.get(tokenURL, function (res) {
              var body = '';
@@ -335,14 +332,14 @@ client.on('msg', msg => {
                  var priceResponse = JSON.parse(body);
                  console.log("Got a response: ", priceResponse);
                  var pricePart = priceResponse.USD;
-	  	 msg.channel.send("The price of Ethereum (ETH) is " + pricePart + "$ per coin")
+	  	 message.channel.send("The price of Ethereum (ETH) is " + pricePart + "$ per coin")
                  console.log("Set Price: ", pricePart);
              });
          }).on('error', (e) => {
              console.error(e);
 	  });
   }
-  if(msg == prefix + "crypto ltc") {
+  if(message == "" + config.prefix + "crypto ltc") {
 	  var tokenURL = 'https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD';
 	  https.get(tokenURL, function (res) {
              var body = '';
@@ -353,14 +350,14 @@ client.on('msg', msg => {
                  var priceResponse = JSON.parse(body);
                  console.log("Got a response: ", priceResponse);
                  var pricePart = priceResponse.USD;
-	  	 msg.channel.send("The price of Litecoin (LTC) is " + pricePart + "$ per coin")
+	  	 message.channel.send("The price of Litecoin (LTC) is " + pricePart + "$ per coin")
                  console.log("Set Price: ", pricePart);
              });
          }).on('error', (e) => {
              console.error(e);
 	  });
    }
-   if(msg == prefix + "crypto bch") {
+   if(message == "" + config.prefix + "crypto bch") {
 	  var tokenURL = 'https://min-api.cryptocompare.com/data/price?fsym=BCH&tsyms=USD';
 	  https.get(tokenURL, function (res) {
              var body = '';
@@ -371,14 +368,14 @@ client.on('msg', msg => {
                  var priceResponse = JSON.parse(body);
                  console.log("Got a response: ", priceResponse);
                  var pricePart = priceResponse.USD;
-	    	 msg.channel.send("The price of Bitcoin Cash (BCH) is " + pricePart + "$ per coin")
+	    	 message.channel.send("The price of Bitcoin Cash (BCH) is " + pricePart + "$ per coin")
                  console.log("Set Price: ", pricePart);
              });
          }).on('error', (e) => {
              console.error(e);
 	  });
    }
-   if(msg == prefix + "crypto xrp") {
+   if(message == "" + config.prefix + "crypto xrp") {
 	  var tokenURL = 'https://min-api.cryptocompare.com/data/price?fsym=XRP&tsyms=USD';
 	  https.get(tokenURL, function (res) {
              var body = '';
@@ -389,7 +386,7 @@ client.on('msg', msg => {
                  var priceResponse = JSON.parse(body);
                  console.log("Got a response: ", priceResponse);
                  var pricePart = priceResponse.USD;
-	  	 msg.channel.send("The price of Ripple (XRP) is " + pricePart + "$ per coin")
+	  	 message.channel.send("The price of Ripple (XRP) is " + pricePart + "$ per coin")
                  console.log("Set Price: ", pricePart);
              });
          }).on('error', (e) => {
@@ -397,4 +394,5 @@ client.on('msg', msg => {
 	  });
    }
 });
+
 client.login(config.token);
