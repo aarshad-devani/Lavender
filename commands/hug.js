5ab7fcb276f7bacb654060e9380a5243dddcb9f1
Matt <wowitbematt@gmail.com>
2018-11-01 20:54:14 -0700

    Fixed package file

---
+++ commands/hug.js
@@ -0,0 +1,13 @@
+const snekfetch = require('snekfetch');
+const config = require('./config.json');
+exports.run = (client, message, args) => {
+    if(message.mentions.users.size < 1) return message.channel.send("You can't hug anybody now.");
+    let user = message.guild.member(message.mentions.users.first());
+    snekfetch.get('https://nekos.life/api/hug').set('Key', config.api1).then(r => message.channel.send(`${user}, you just got hugged by ${message.author.username}`, {
+        embed: {
+            image: {
+                url: r.body.url
+            }
+        }
+    }));
+}
