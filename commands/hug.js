12c86a3d89e3168d996551f4c5804f5bf6f97e63
= <ultramlg1108@gmail.com>
2018-11-04 10:56:33 -0800

    Fix config not found (in hug.js) and export name

--- commands/hug.js
+++ commands/hug.js
@@ -1,6 +1,6 @@
 const snekfetch = require('snekfetch');
-const config = require('./config.json');
-exports.run = (client, message, args) => {
+const config = require('../config.json');
+module.exports.run = (client, message, args) => {
     if(message.mentions.users.size < 1) return message.channel.send("You can't hug anybody now.");
     let user = message.guild.member(message.mentions.users.first());
     snekfetch.get('https://nekos.life/api/hug').set('Key', config.api1).then(r => message.channel.send(`${user}, you just got hugged by ${message.author.username}`, {
@@ -10,4 +10,8 @@ exports.run = (client, message, args) => {
             }
         }
     }));
+}
+
+module.exports.help = {
+	name: "hug"
 }
