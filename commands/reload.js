83eaa687c1c95a434f5191157107725a38433825
Ultra03 <20672260+Ultra03@users.noreply.github.com>
2018-11-13 16:07:05 -0800

    Check if the reload command author is a bot dev (#74)
    * Check if the reload command author is a bot dev
    
    * Fix logic for checking user IDs on reload command

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,7 @@
 module.exports = {
   run: function (client, message, args) {
-    if (args.size < 1 || args.size === 0) return message.reply("Must provide a command name to reload.");
+    if(message.author.id != '341748916829552641' && message.author.id != '193493980611215360') return message.reply("You don't have permission to run that command!");
+    if (args.length < 1 || args.length === 0) return message.reply("Must provide a command name to reload.");
     delete require.cache[require.resolve(`./${args[0]}.js`)];
     message.reply(`The command ${args[0]} has been reloaded`);
   },
