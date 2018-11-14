137879be3af61ee760734d4b17565f32b0cb4125
Matt <wowitbematt@gmail.com>
2018-11-13 19:33:41 -0800

    Caught up with latest

--- serverConf/default.js
+++
@@ -1,8 +0,0 @@
-const config = require("../config.json");
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const Enmap = require('enmap');
-
-
-// Use, for example, 'guildConf.prefix' instead of 'config.prefix'. Add more
-// values to the 'default settings section to allow new features.
