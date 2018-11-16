fc4ad52ece82e46b331fc3b94ed60206db373dda
Matt <wowitbematt@gmail.com>
2018-11-15 16:30:47 -0800

    Added Webhook and Intervals for DBL, Discord Bots, and DDB

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,28 @@
+const config = require('../config.json');
+const snekfetch = require('snekfetch');
 exports.run = (client) => {
     console.log("Logged in as Lavender");
-    client.user.setActivity("Use +help");
+    client.user.setActivity(`${config.prefix}help | ${client.guilds.size} servers`);
     console.log("Presence Changed Successfully.");
     console.log("You are currently using version 1.1-rc. A fair warning that this is a development build and not meant for production. Use at your own \"risk\"");
     console.log("-------");
+    setInterval(() => {
+        snekfetch.post(`https://divineddiscordbots.com/bots/${client.user.id}/stats`, {
+            header: { Authorization: config.dbotsToken3 }
+        }).send({
+            server_count: client.guilds.size
+        }).catch(r => console.log('[divineddiscordbots.com] Failed POST'));
+
+        snekfetch.post(`https://discordbotlist.com/api/bots/${client.user.id}/stats`, {
+            header: { Authorization: 'Bot ' + config.dbotsToken2 }
+        }).send({
+            guilds: client.guilds.size
+        }).catch(r => console.log('[discordbotlist.com] Failed POST'));
+        
+        snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`, {
+            header: { Authorization: config.dbotsToken1 }
+        }).send({
+            server_count: client.guilds.size
+        }).catch(r => console.log('[discordbots.org] Failed POST'));
+    })
 }
