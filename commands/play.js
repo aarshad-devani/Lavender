d79496137739766d104cdac17e324d2b3edd4e19
Matt <wowitbematt@gmail.com>
2019-02-05 06:27:10 -0800

    Attempted to Repair Play Logic and Added Error Catching

--- commands/play.js
+++ commands/play.js
@@ -3,6 +3,7 @@ const ytdl = require("ytdl-core");
 module.exports = {
     run: function (client, message, args, ytdl) {
         console.log("Got a song request!");
+        const streamOptions = { seek: 0, volume: 1};
         const voiceChannel = message.member.voiceChannel;
         const url = args[0];
         if (!voiceChannel) {
@@ -10,11 +11,11 @@ module.exports = {
         }
         voiceChannel.join().then(connection => {
             const stream = ytdl(url, { filter: "audioonly" });
-            const dispatcher = connection.playStream(stream);
-            dispatcher.on("end", () => {
+            const dispatcher = connection.playStream(stream, streamOptions);
+            dispatcher.on("end", end => {
                 voiceChannel.leave();
             });
-        });
+        }).catch(err => console.log(err));
     },
 
     help: {
