4cbb3e7d6baddc204d8684a10e833edee0faf339
Matt <wowitbematt@gmail.com>
2019-02-22 12:07:46 -0800

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -3,8 +3,12 @@ const config = require("../config.json");
 module.exports = {
     run: function (client, message, args) {
         let user = message.guild.member(message.mentions.users.first());
-        if (message.mentions.users.size < 1) {
-            message.channel.send("You can't hug anybody now.");
+        if (message.mentions.users.size > 1) {
+            message.channel.send("You can't hug more than one person at a time!");
+        } else if (message.author.id === user) {
+            message.channel.send("You can't hug yourself.");
+        } else if (message.mentions.users.size < 1) {
+            message.channel.send("You can't hug no one, silly");
         } else {
             snekfetch.get("https://nekos.life/api/hug").then(r => message.channel.send(`${user}, you just got hugged by ${message.author.username}`, {
                 embed: {
