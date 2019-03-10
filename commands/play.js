8157a1cc65e28fd70558d4c9fdce23bdf925761c
Matt <wowitbematt@gmail.com>
2019-03-10 14:27:05 -0700

    Official Mirror for 2.2 (develop)

--- commands/play.js
+++ commands/play.js
@@ -1,20 +1,31 @@
 const config = require("../config.json");
 const ytdl = require("ytdl-core");
 module.exports = {
-    run: function (client, message, args, ytdl) {
+    run: function (client, message, args) {
         console.log("Got a song request!");
+        const streamOptions = { seek: 0, volume: 1};
         const voiceChannel = message.member.voiceChannel;
         const url = args[0];
         if (!voiceChannel) {
             return message.reply("Please get in a voice channel so I can blast the good vibes!");
         }
-        voiceChannel.join().then(connection => {
+        voiceChannel.join().then(function(connection) {
             const stream = ytdl(url, { filter: "audioonly" });
-            const dispatcher = connection.playStream(stream);
-            dispatcher.on("end", () => {
+            const dispatcher = connection.playStream(stream, streamOptions);
+            connection.on('error', (err) => {
+                console.log("An error occurred! | ", err);
+            });
+            connection.on('disconnected', (err) => {
+                console.log("Lavender seems to have unplugged something on accident! | ", err);
+            });
+            dispatcher.on("end", end => {
                 voiceChannel.leave();
             });
+        }).catch(function(err) {
+            console.log(err);
         });
+
+        
     },
 
     help: {
--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ const scheduler = require("node-schedule");
 
 const invites = {};
 
-const wait = require('util').promisify(setTimeout);
+const wait = require("util").promisify(setTimeout);
 
 module.exports.run = (client) => {
     wait(1000);
@@ -18,35 +18,35 @@ module.exports.run = (client) => {
     console.log(`Logged in as ${client.user.username}`);
     client.user.setActivity(`${config.prefix}help | ${client.guilds.size} servers`);
     console.log("Presence Changed Successfully.");
-    scheduler.scheduleJob('*/15 * * * *', function() {
+    scheduler.scheduleJob("*/15 * * * *", function() {
         client.user.setActivity(`${config.prefix}help | ${client.guilds.size} servers`);
         console.log("Updated server amount.");
     });
-    console.log("Presence scheduler enabled.");
-    console.log("You are currently using version 1.1.2-rc. A fair warning that this is a development build and not meant for production. Use at your own \"risk\"");
+    console.log("Presence Scheduler enabled.");
+    console.log("You are currently using version 2.1-b. A fair warning that this is a development build and not meant for production. Use at your own \"risk\"");
     console.log("-------");
     if(config.useDbots) {
         const dbl = new DBL(config.dbotsToken1, client);
         setInterval(() => {
-            snekfetch.post(`https://divineddiscordbots.com/bots/493955692445696015/stats`, {
+            snekfetch.post("https://divineddiscordbots.com/bots/493955692445696015/stats", {
                 headers: { Authorization: `${config.dbotsToken3}` }
             }).send({
                 server_count: client.guilds.size
-            }).catch(r => console.log('[divineddiscordbots.com] Failed POST'));
+            }).catch(r => console.log("[divineddiscordbots.com] Failed POST"));
     
-            console.log('Updating DiscordBotList.com stats');
-            snekfetch.post(`https://discordbotlist.com/api/bots/493955692445696015/stats`).set("Authorization", `Bot ${config.dbotsToken3}`).send({
+            console.log("Updating DiscordBotList.com stats");
+            snekfetch.post("https://discordbotlist.com/api/bots/493955692445696015/stats").set("Authorization", `Bot ${config.dbotsToken3}`).send({
                 shard_id: 0,
                 guilds: client.guilds.size,
                 users: client.users.size,
-                voice_connections: client.voiceConnactions.size
-            }).catch(r => console.log('[discordbotlist.com] Failed POST'));
+                voice_connections: client.voiceConnections.size
+            }).catch(r => console.log("[discordbotlist.com] Failed POST"));
         }, 3600000);
-        dbl.on('posted', () => {
+        dbl.on("posted", () => {
             console.log(`The server count of ${client.guilds.size} has been posted to DiscordBots.org`);
         });
-        dbl.on('error', e => {
+        dbl.on("error", e => {
             console.log(`Oops! ${e}`);
         });
     } 
-}
+};
