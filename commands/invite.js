dde107f49fd57bd7e26e126c5c02018697e8d392
Matt <wowitbematt@gmail.com>
2019-03-10 14:07:17 -0700

    Repaired ID Fetching for invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports = {
     run: function (client, message, args) {
-        var botID = Discord.ClientUser.id;
+        var botID = client.user.id;
         message.channel.send(`Help spread the love of Lavender to another server by sharing this bot link: https://discordapp.com/oauth2/authorize?client_id=${botID}&permissions=36924486&scope=bot`);
     },
     help: {
