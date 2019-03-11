28e7551157fe84ce7828adcf21ee713cfc74c8a5
Matt <wowitbematt@gmail.com>
2019-03-10 19:06:30 -0700

    Added Return Statement on Reloading

--- commands/reload.js
+++ commands/reload.js
@@ -2,9 +2,11 @@ module.exports = {
   run: function (client, message, args) {
     if(message.author.id !== "341748916829552641" && message.author.id !== "193493980611215360") {
       message.reply("You don't have permission to run that command!");
+      return;
     }
     if (args.length < 1 || args.length === 0) {
       message.reply("Must provide a command name to reload.");
+      return;
     }
     delete require.cache[require.resolve(`./${args[0]}.js`)];
     message.reply(`The command ${args[0]} has been reloaded`);
