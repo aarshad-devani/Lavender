6db3762cd4e8e6d8beb2cf127cd93c8f0009b02f
Matt <wowitbematt@gmail.com>
2019-03-10 19:05:40 -0700

    Added Eight-Ball Null Response

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,6 +1,9 @@
 module.exports.run = (client, message, args) => {
-		var responses = ["It is certain", "Without a doubt", "You may rely on it", "Most likely", "Yes", "Signs point to yes", "Better not tell you now", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
-		message.channel.send(":8ball: " + responses[Math.floor(Math.random() * (responses.length))]);
+	if(args === undefined) {
+		message.channel.send("The eight-ball is broken. Maybe try telling it something?");
+	}	
+	var responses = ["It is certain", "Without a doubt", "You may rely on it", "Most likely", "Yes", "Signs point to yes", "Better not tell you now", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+	message.channel.send(":8ball: " + responses[Math.floor(Math.random() * (responses.length))]);
 };
 
 module.exports.help = {
