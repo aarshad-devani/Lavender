307423f76b19943fdb2c48095a1a32012029e88e
Matt <wowitbematt@gmail.com>
2019-03-10 20:43:43 -0700

    Added Dog API

---
+++ commands/dog.js
@@ -0,0 +1,16 @@
+const snekfetch = require("snekfetch");
+const config = require("../config.json");
+module.exports = {
+    run: function(client, message, args) {
+        snekfetch.get("https://dog.ceo/api/breeds/image/random").then(r => message.channel.send(`Here's a cutie for you, ${message.author.username}`, {
+            embed: {
+                image: {
+                    url: r.body.message
+                }
+            }
+        }));
+    },
+    help: {
+        name: "hug"
+    }
+};
