7d3d9c4a529cd5a88d41cfb2b0d8043a227c4a6c
Matt <wowitbematt@gmail.com>
2019-03-10 20:00:00 -0700

    Added Base YouTube URL Start Detection

--- commands/play.js
+++ commands/play.js
@@ -2,18 +2,22 @@ const config = require("../config.json");
 const ytdl = require("ytdl-core");
 module.exports = {
     run: function (client, message, args) {
-        console.log("Got a song request!");
         const streamOptions = { seek: 0, volume: 1};
         const voiceChannel = message.member.voiceChannel;
-        const url = args[0];
+        var url = args[0];
         if (!voiceChannel) {
             return message.reply("Please get in a voice channel so I can blast the good vibes!");
         }
         if(!url) {
             return message.reply("I need a song to play.");
         }
+        if(!url.startsWith() === "https://www.youtube.com" || !url.startsWith() === "http://www.youtube.com" || !url.startsWith() === "https://youtube.com" || !url.startsWith() === "http://youtube.com") {
+            url2 = "https://youtube.com/" + url;
+        } else {
+            url2 = url;
+        }
         voiceChannel.join().then(function(connection) {
-            const stream = ytdl(url, { filter: "audioonly" });
+            const stream = ytdl(url2, { filter: "audioonly" });
             const dispatcher = connection.playStream(stream, streamOptions);
             connection.on('error', (err) => {
                 console.log(`An error occurred! | ${err}`);
