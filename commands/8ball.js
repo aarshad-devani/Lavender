31b2f7c15c8f676254d9f646af104d2805256324
Matt <wowitbematt@gmail.com>
2019-03-10 19:37:32 -0700

    Revised Config Files

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,6 +1,6 @@
 module.exports.run = (client, message, args) => {
 	let str = args.toString();
-	if(str === undefined) {
+	if(str == undefined) {
 		message.channel.send("The eight-ball is broken. Maybe try telling it something?");
 	}	
 	var responses = ["It is certain", "Without a doubt", "You may rely on it", "Most likely", "Yes", "Signs point to yes", "Better not tell you now", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
