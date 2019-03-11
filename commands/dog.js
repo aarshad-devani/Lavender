c1926f1119740fd09a9b36c4737f29891efb81a0
Matt <wowitbematt@gmail.com>
2019-03-10 21:30:38 -0700

    Fixed Call Names

--- commands/dog.js
+++ commands/dog.js
@@ -11,6 +11,6 @@ module.exports = {
         }));
     },
     help: {
-        name: "hug"
+        name: "dog"
     }
 };
--- commands/mute.js
+++ commands/mute.js
@@ -27,6 +27,6 @@ module.exports = {
         }
     },
     help: {
-        name: "hug"
+        name: "mute"
     }
 };
