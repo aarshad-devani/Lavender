3b5b173699b524f78c40cb9bd8232366ca547a40
Matt <wowitbematt@gmail.com>
2019-03-10 20:58:42 -0700

    Revised Temp Ban Permissions

--- commands/tempban.js
+++ commands/tempban.js
@@ -4,6 +4,18 @@ module.exports = {
         if(!member.bannable) {
             message.reply(`${member.user.tag} is too powerful for me to beat!`);
         } 
+        if(!message.member.hasPermission("BAN_MEMBERS")) {
+          message.reply("You don't have the permission '**BAN_MEMBERS**'");
+          return;
+        }
+        if(message.mentions.members.size === 0) {
+          message.reply("Please mention a user to ban.");
+          return;
+        }
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
+          message.reply("I don't have permission to ban members!");
+          return;
+        }
         if(message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member.user.tag} is too powerful for you to temporarily beat 'em.`);
         let txt = `Are you sure you want to tempban ${member.user.tag}?`;
         const m = message.channel.reply(message.author, txt);
