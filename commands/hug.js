7fa27666e70c9dbc11300fb7952e8a2a29720a9d
Matt <wowitbematt@gmail.com>
2019-03-10 19:08:31 -0700

    Added Logic and Return Statements to Hugging

--- commands/hug.js
+++ commands/hug.js
@@ -3,7 +3,14 @@ const config = require("../config.json");
 module.exports = {
     run: function (client, message, args) {
         if (message.mentions.users.size < 1) {
-            message.channel.send("You can't hug anybody now.");
+            message.channel.send("You can't hug nobody.");
+            return;
+        } else if (message.mentions.user.first() === message.author) {
+            message.channel.send("You can't hug yourself.");
+            return;
+        } else if (message.mentions.users.size > 1) {
+            message.channel.send("You can't hug more than one person at a time!");
+            return;
         }
         let user = message.guild.member(message.mentions.users.first());
         snekfetch.get("https://nekos.life/api/hug").then(r => message.channel.send(`${user}, you just got hugged by ${message.author.username}`, {
