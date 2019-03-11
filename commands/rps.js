a85d5a738d24915c7a0f64e10bacac944dad8a27
Matt <wowitbematt@gmail.com>
2019-03-10 19:26:38 -0700

    Added Return Statements to RPS

--- commands/rps.js
+++ commands/rps.js
@@ -19,14 +19,18 @@ module.exports = {
             if(str2 === "s" && cPick === "r" || str2 === "r" && cPick === "p" || str2 === "p" && cPick === "s") {
                 // CPU Wins, Yay?
                 message.channel.send("Haha! I did it. Gotcha. Beat you. Boom.");
+                return;
             } else if(str2 === "s" && cPick === "p" || str2 === "r" && cPick === "s" || str2 === "p" && cPick === "r") {
                 // User Wins, Yay!
                 message.channel.send("Good job, you won! And to think I had you.");
+                return;
             } else if(str2 === cPick) {
                 // Draw
                 message.channel.send("Dang, we did the same thing.");
+                return;
             } else {
                 message.channel.send("Uhh... I'mma just back out of the room now.. uh.. bye.");
+                return;
             }
         });
         
