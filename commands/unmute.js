dda5aecb4007e5822e63e8313f18b8a59dc60da5
Matt <wowitbematt@gmail.com>
2019-03-11 08:30:23 -0700

    Created Unmute to Counter Mute Spells

---
+++ commands/unmute.js
@@ -0,0 +1,30 @@
+module.exports = {
+    run: function(client, message, args) {
+        var [member, duration] = message.args;
+        if(!message.member.hasPermission("MANAGE_ROLES")) {
+          message.reply("You don't have the permission '**MANAGE_MESSAGES**'");
+          return;
+        }
+        if(message.mentions.members.size === 0) {
+          message.reply("Please mention a user to unmute.");
+          return;
+        }
+        if(!message.guild.me.hasPermission("MANAGE_ROLES")) {
+          message.reply("I can't manage messages, therefore");
+          return;
+        }
+        if(message.member.roles.highest.position <= member.roles.highest.position) {
+            message.reply(`${member.user.tag} is too powerful for you to unmute them.`);
+        }
+        channel.replacePermissionOverwrites({
+            overwrites: [{
+                id: member,
+                allowed: ['SEND_MESSAGES'],
+            }];
+        }).catch(err => { return console.log(err) });
+        message.channel.send(`${member} was unmuted by ${message.author.tag}.`);
+    },
+    help: {
+        name: "unmute"
+    }
+};
