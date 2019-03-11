e9c806b806aba5aae372b0e81dac9a94a69c5c6b
Matt <wowitbematt@gmail.com>
2019-03-10 20:09:02 -0700

    Added Return Statement to Eight-Ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,6 +2,7 @@ module.exports.run = (client, message, args) => {
 	let str = args.toString();
 	if(!str) {
 		message.channel.send("The eight-ball is broken. Maybe try telling it something?");
+		return;
 	}	
 	var responses = ["It is certain", "Without a doubt", "You may rely on it", "Most likely", "Yes", "Signs point to yes", "Better not tell you now", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
 	message.channel.send(":8ball: " + responses[Math.floor(Math.random() * (responses.length))]);
