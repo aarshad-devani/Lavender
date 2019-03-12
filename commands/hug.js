ad301229792ed8404422836fb50acc29c62ba799
Matt <wowitbematt@gmail.com>
2019-03-12 06:48:56 -0700

    Added Hug by Lavender

--- commands/hug.js
+++ commands/hug.js
@@ -12,6 +12,17 @@ module.exports = {
             message.channel.send("You can't hug more than one person at a time!");
             return;
         }
+        let argument = args.slice(1).join(" ");
+        let argu = argument.toUpperCase();
+        if(argu === "ME"){
+            return snekfetch.get("https://nekos.life/api/hug").then(r => message.channel.send(`Here, ${message.author.username}, have a hug, since you asked.`, {
+                embed: {
+                    image: {
+                        url: r.body.url
+                    }
+                }
+            }));
+        }
         let user = message.guild.member(message.mentions.users.first());
         snekfetch.get("https://nekos.life/api/hug").then(r => message.channel.send(`${user}, you just got hugged by ${message.author.username}`, {
             embed: {
