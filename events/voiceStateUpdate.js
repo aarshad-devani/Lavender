2f954d65e7d2fc681b50bea18b9bc10c77c7c715
= <ultramlg1108@gmail.com>
2019-05-09 16:43:13 -0700

    Can't believe I overlooked this... (resolves #113)

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,19 +1,25 @@
 module.exports.run = (client, oldMember, newMember) => {
     let oldChannel = oldMember.voiceChannel;
     let newChannel = newMember.voiceChannel;
-
-    if(newChannel !== oldChannel) {
+	
+	if(oldChannel != undefined) {
+		// Make sure they didn't just join a channel
+		
+		if(newChannel !== oldChannel) {
         // User left a voice channel
+		
+			if(oldChannel.members.has(client.user.id)) {
+				// User left a channel that we're in
 
-        if(oldChannel.members.has(client.id)) {
-            // User left a channel that we're in
+				if(oldChannel.members.size == 1) {
+					// We're the only ones left in the channel
 
-            if(oldChannel.size == 1) {
-                // We're the only ones left in the channel
-
-                // Leave the channel after 10 seconds
-                setTimeout(oldChannel.leave(), 10000);
-            }
-        }
-    }
+					// Leave the channel after 10 seconds
+					setTimeout(() => {
+						oldChannel.leave();
+					}, 10000);
+				}
+			}
+		}
+	}
 }
